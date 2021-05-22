5d4a9a0ac86c1a387bdc22b66b2c70f1f6945e4d
siha1 <mapelamll@naver.com>
2021-05-22 23:28:06 +0900

    자료 모두 read 해서 뿌리기

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,9 +1,52 @@
 <template>
-    <div>hif</div>
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+            <add-item-form />
+        </div>
+        <list-view :items="items" />
+    </div>
 </template>
 
 <script>
-export default {};
+import addItemForm from "./addItemForm.vue";
+import listView from "./listView.vue";
+
+export default {
+    components: { addItemForm, listView },
+    data: function() {
+        return {
+            items: []
+        };
+    },
+    methods: {
+        getList() {
+            axios
+                .get("api/items")
+                .then(res => {
+                    this.items = res.data;
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    },
+    created() {
+        this.getList();
+    }
+};
 </script>
 
-<style></style>
+<style scoped>
+.todoListContainer {
+    width: 350px;
+    margin: auto;
+}
+.heading {
+    background: #e6e6e6;
+    padding: 10px;
+}
+#title {
+    text-align: center;
+}
+</style>
